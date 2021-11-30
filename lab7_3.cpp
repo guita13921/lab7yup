7b9fb4fea0b518c8dbdb5d5b462ed242fca955ca
guita13921 <guita13921@gmail.com>
2021-11-30 17:59:35 +0700

    lab7_3.cpp complete

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,39 @@
 #include<iostream>
 
 using namespace std;
+int adiff(int A,int B){
+    while(A > 360){
+        A = A - 360;
+    }
+    while(B > 360){
+        B = B -360;
+    }
+    while(A < 0){
+    A = 360 + A;
+    }
+    while(B < 0){
+    B = 360 + B;
+    }
+int x;
+      x = A-B;
+          if(x < 0){
+          x = -x;
+          }
+          if(x > 180){
+          x = 360 - x;}
+  return x;
+}
+
+
 
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  cout << adiff(180,270) << "\n";
+  cout << adiff(210,45) << "\n";
+  cout << adiff(0,360) << "\n";
+  cout << adiff(10,350) << "\n";
+  cout << adiff(95,260) << "\n";
+  cout << adiff(90,-90) << "\n";
+  cout << adiff(1000,280) << "\n";
+  cout << adiff(60,244) << "\n";
 }
